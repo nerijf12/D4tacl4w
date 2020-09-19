26fa97dca7da8d83cbc06f6152f19abec3addb26
kayla <kmbicol@gmail.com>
2020-09-19 15:56:28 -0500

    added Dataclaw class and refined some of the preprocessing of csvs

--- _py/find_errCSVfiles.py
+++ _py/find_errCSVfiles.py
@@ -1,39 +1,38 @@
-# import clevercsv as csv
 import os
 import glob
 import pandas as pd
-
 os.chdir("../_propcsv")
 
 extension = 'csv'
 all_filenames = glob.glob('*.{}'.format(extension))
 all_filenames.sort(key=os.path.getmtime)
-errorfiles = []
 
-# combine all files in the list
+
+
+# getting column headings of known correct csv
+df = pd.read_csv(all_filenames[0])
+headings = list(df.columns)
+
+frames = []
+
+# go through and find which csv cannot become df
 for f in all_filenames:
     # print(f)
     try:
         a = pd.read_csv(f)
-        if a.empty:
-            # print("File " + str(all_filenames.index(f)+1) + "is empty. Skipping...")
+        if a.empty: #skip empty
             continue
-        # print(a.head())
-    except:
-        # print(f)
-        errorfiles.append(f)
-        # print(f"{f}\n{len(pd.read_csv(f).columns)}.")
-    # print(a)
+    except: # checks to make df out of each of csvs in errorfiles
+        # collect all bad csvs
+        a = pd.read_csv(f, names=headings)  # idk why but this fixes it
+        a = a.drop(a.index[0])
+    a['Zipcode'] = f.split('.')[-2]
+    frames.append(a)
 
-print(errorfiles)
-print(len(errorfiles))
+combined_df = pd.concat(frames)
 
-# getting column headings
-df = pd.read_csv(all_filenames[0])
-headings = list(df.columns)
 
-# checks to make df out of each of csvs in errorfiles
-for myerr in errorfiles:
-    b = pd.read_csv(myerr, names=headings)  # idk why but this fixes it
-    newb = b.drop(b.index[0])
-    print(newb.head())
+# for myerr in errorfiles:
+#     b = pd.read_csv(myerr, names=headings)  # idk why but this fixes it
+#     newb = b.drop(b.index[0])
+#     print(newb.head())
