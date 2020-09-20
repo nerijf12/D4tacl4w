4a0b658402f18fc80b70cb128073627fa314ee21
Juan Neri <nerijf12@gmail.com>
2020-09-19 19:52:56 -0500

    created the zipcsv files

--- _py/htmltocsv_zip.py
+++ _py/htmltocsv_zip.py
@@ -1,29 +1,45 @@
 from lxml import html
-import requests
-import csv
+import requests, csv, os, os.path
 
-zipcode = "zipcode.csv"
-page = requests.get('http://bexar.trueautomation.com/clientdb/Property.aspx?cid=110&prop_id=1275766')
-table = html.fromstring(page.content)
-index = 0
-startingNum = 2
+DIR = '../_propcsv_zipnames/'
+ZIPCSV = '../_zipcsv/'
+list = os.listdir(DIR)
+# number_files = len(list)
+# print(number_files)
 
-#This will create a list of buyers:
-taxrate = table.xpath('//table[@class="tableData"]/tr/td//text()')
-#This will create a list of prices
-description = table.xpath('//tr/td[@class="propertyTJDescription"]//text()')
+for zipcode in list:
+	index = 0
+	startingNum = 2
+	# file = open(zipcode, "r")
+	with open(DIR + zipcode) as csv_file:
+		reader = csv.reader(csv_file, delimiter=',')
+		for row in reader:
+			if row[0] == 'Property ID':
+				continue
+			else:
+				property_id = row[0]
 
-f = open(zipcode, "a")
+				page = requests.get('http://bexar.trueautomation.com/clientdb/Property.aspx?cid=110&prop_id=' + property_id)
+				table = html.fromstring(page.content)
 
-for title in description:
-	if index < 1:
-		together = title + ", " + taxrate[startingNum] + "\n"
-		# print(title, taxrate[startingNum])
-		f.write(together)
-	else:
-		startingNum = startingNum + 8
-		together = title + ", " + taxrate[startingNum] + "\n"
-		f.write(together)
-		# print(title, taxrate[startingNum])
-	index += 1
-f.close()
+				#This will create a list of buyers:
+				taxrate = table.xpath('//table[@class="tableData"]/tr/td//text()')
+				#This will create a list of prices
+				description = table.xpath('//tr/td[@class="propertyTJDescription"]//text()')
+
+				f = open(ZIPCSV + zipcode, "a")
+
+				for title in description:
+					if index < 1:
+						together = title + ", " + taxrate[startingNum] + "\n"
+						f.write(together)
+					else:
+						startingNum += 8
+						together = title + ", " + taxrate[startingNum] + "\n"
+						f.write(together)
+					index += 1
+
+				f.close()
+			break
+
+	csv_file.close()
